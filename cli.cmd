73d96a736dd418f031e51286fea330b7a2aa0e25
Jagpreet Singh Tamber <jagpreetstamber@gmail.com>
2021-02-10 18:19:04 +0530

    Update and rename cli.ps1 to cli.cmd

---
+++ cli.cmd
@@ -0,0 +1,11 @@
+echo "Hello"
+echo %PATH%
+
+echo "Try the az"
+az --version
+
+echo "Try full path"
+c:\AzCLI\az.cmd --version
+
+echo "Try full path without cmd"
+c:\AzCLI\az --version
--- cli.ps1
+++
@@ -1,8 +0,0 @@
-echo "Hello"
-echo %PATH%
-
-echo "Try the az"
-az --version
-
-echo "Try full path"
-c:\Azcli\az.cmd --version
