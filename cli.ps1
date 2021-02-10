4b130ef3f5e5862929a4fa8523eb263f15e984c6
Jagpreet Singh Tamber <jagpreetstamber@gmail.com>
2021-02-10 15:22:59 +0530

    Update cli.ps1

--- cli.ps1
+++ cli.ps1
@@ -1,9 +1,10 @@
 Write-Output "Hello"
 Write-Output $env:PATH
-$a = az --version
-
-Write-Output $a
+az --version
 
 Write-Output "Testing PATH"
 Test-Path -Path C:\AzCli\az.cmd -PathType Leaf
 Write-Error "I am Error"
+
+
+c:\Azcli\az.cmd --version
