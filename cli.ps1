9e6a727d8d6bd05437bc95ea6ca90ca774548c41
Jagpreet Singh Tamber <jagpreetstamber@gmail.com>
2021-02-10 15:30:32 +0530

    Update cli.ps1

--- cli.ps1
+++ cli.ps1
@@ -4,6 +4,7 @@ az --version
 
 Write-Output "Testing PATH"
 Test-Path -Path C:\AzCli\az.cmd -PathType Leaf
+Test-Path -Path "C:\WPy64-3800\python-3.8.0.amd64\python.exe" -PathType Leaf 
 Write-Error "I am Error"
 
 
